cba9a2c4f75caf0cde8757ef6c2e50dc481e5e4b
MDAmir159 <islamamirul497@gmail.com>
2023-04-15 19:38:47 +0600

    databaseConfig.js updated

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const { USER, HOST, CONNECTION_LIMIT, PASSWORD, DATABASE } = require('.');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     connectionLimit : CONNECTION_LIMIT,
     host : HOST,
     user : USER,
@@ -9,11 +9,11 @@ const db = mysql.createConnection({
     database : DATABASE
 })
 
-db.connect(function(error){
+db.getConnection(function(error, connection){
     if(error){
         console.log(error);
     }else{
-        console.log('connected as ' + db.threadId);
+        console.log('connected as ' + connection.threadId);
     }
 })
 
